a182ea42f197251e3321fc4c64f0cc879f9de9f2
Savinov Evgeny <Zsavinoww@gmail.com>
2020-04-12 14:32:16 +0300

    first multithreading application

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Main {
+    public static void main(String[] args) {
+        ThreadPoolExecutor ex = (ThreadPoolExecutor) Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        ReadFile rf = new ReadFile(ex);
+        ArrayList<String> listFilePath = new ArrayList<>();
+        listFilePath.add("/Users/user/IdeaProjects/multithreading/src/test");
+        listFilePath.add("/Users/user/IdeaProjects/multithreading/src/test1.txt");
+        listFilePath.add("/Users/user/IdeaProjects/multithreading/src/test2.txt");
+        listFilePath.add("/Users/user/IdeaProjects/multithreading/src/test3.txt");
+        for (String path : listFilePath) {
+            rf.read(path);
+        }
+        ex.shutdownNow();
+    }
+}
---
+++ src/ReadFile.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ReadFile {
+
+    private ThreadPoolExecutor ex;
+
+    public ReadFile(ThreadPoolExecutor ex) {
+        this.ex = ex;
+    }
+
+    public void read(final String path) {
+            ex.execute(() -> {
+                File file = new File(path);
+                if (!file.isFile()){
+                    System.out.println("Это не файл " + file.getName());
+                    return ;
+                }
+                System.out.println("Файл " + file.getName() + " имеет размер " + file.length() + " bytes");
+                String substring = path.indexOf('.') >= 0 ? path.substring(path.indexOf('.')) : null;
+                System.out.println("Файл " + file.getName() + " имеет расширение " + substring);
+            });
+    }
+}
