431825088e18f684641d0e12735032d55035b28f
Savinov Evgeny <Zsavinoww@gmail.com>
2020-04-13 09:19:26 +0300

    add semaphore synchronized

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.Lock;
 
 public class Main {
     public static void main(String[] args) {
         ThreadPoolExecutor ex = (ThreadPoolExecutor) Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        ReadFile rf = new ReadFile(ex);
+        Semaphore sm = new Semaphore(1);
+        ReadFile rf = new ReadFile(ex, sm);
         ArrayList<String> listFilePath = new ArrayList<>();
         listFilePath.add("/Users/user/IdeaProjects/multithreading/src/test");
         listFilePath.add("/Users/user/IdeaProjects/multithreading/src/test1.txt");
--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,26 +1,37 @@
 import java.io.File;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.Lock;
 
 public class ReadFile {
 
     private ThreadPoolExecutor ex;
+    private Semaphore sm;
 
-    public ReadFile(ThreadPoolExecutor ex) {
+    public ReadFile(ThreadPoolExecutor ex, Semaphore sm) {
         this.ex = ex;
+        this.sm = sm;
     }
 
     public void read(final String path) {
         ex.execute(() -> {
-            synchronized (this) {
+//            synchronized (this) {
+            try {
+                sm.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             File file = new File(path);
+            System.out.println(this);
             if (!file.isFile()) {
                 System.out.println("Это не файл " + file.getName());
                 return;
             }
-                System.out.println("Файл " + file.getName() + " имеет размер " + file.length() + " bytes");
-                String substring = path.indexOf('.') >= 0 ? path.substring(path.indexOf('.')) : null;
-                System.out.println("Файл " + file.getName() + " имеет расширение " + substring);
-            }
+            System.out.println("Файл " + file.getName() + " имеет размер " + file.length() + " bytes");
+            String substring = path.indexOf('.') >= 0 ? path.substring(path.indexOf('.')) : null;
+            System.out.println("Файл " + file.getName() + " имеет расширение " + substring);
+            sm.release();
+//            }
         });
     }
 }
